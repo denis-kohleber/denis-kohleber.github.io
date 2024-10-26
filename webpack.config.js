2036b47a207202587f366381f8b1997987fa1412
Denis-K0 <160408077+Denis-K0@users.noreply.github.com>
2024-10-26 10:19:15 +0200

    Hinzufügung der robots.txt

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     mode: "production",
@@ -55,6 +56,11 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'style.css',
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: 'src/robots.txt', to: '' },
+            ],
+          }),
     ],
     module: {
         rules: [
